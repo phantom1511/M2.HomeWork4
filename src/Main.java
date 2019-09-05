9892419d8eafeb6de733aae57e42116dadcef6df
Dastan Tulokulov <dastan.tulokulov.kg@mail.ru>
2019-09-05 17:51:52 +0600

    Maybe prokatit)

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,13 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Запуск главного потока " + Thread.currentThread().getName());
         Runner runner = new Runner("Runner ");
-        Thread th = new Thread(runner);
-        th.start();
-        try {
-            th.join();
-        } catch (InterruptedException ie) {
-        }
-        System.out.println("Завершение главного потока " + Thread.currentThread().getName());
+        runner.start();
+//        Thread th = new Thread(runner);
+//        th.start();
+//        try {
+//            th.join();
+//        } catch (InterruptedException ie) {
+//        }
+//        System.out.println("Завершение главного потока " + Thread.currentThread().getName());
     }
 }
--- src/Runner.java
+++ src/Runner.java
@@ -1,15 +1,16 @@
-public class Runner extends Thread implements Runnable {
+public class Runner extends Thread {
 
     public Runner(String name) {
+
         super(name);
     }
 
     public void run() {
-        Runner runner1 = new Runner("Runner 1 ");
-        Runner runner2 = new Runner("Runner 2 ");
-        Runner runner3 = new Runner("Runner 3 ");
-        Runner runner4 = new Runner("Runner 4 ");
-        Runner runner5 = new Runner("Runner 5 ");
+        Thread runner1 = new Thread("Runner 1 ");
+        Thread runner2 = new Thread("Runner 2 ");
+        Thread runner3 = new Thread("Runner 3 ");
+        Thread runner4 = new Thread("Runner 4 ");
+        Thread runner5 = new Thread("Runner 5 ");
 
         System.out.println(runner1.getName() + " берет палочку");
         try {
@@ -20,9 +21,6 @@ public class Runner extends Thread implements Runnable {
 
         System.out.println(runner2.getName() + " берет палочку");
         try {
-            Thread th2 = new Thread(runner2);
-            th2.start();
-            th2.join();
             sleep(5000);
         } catch (InterruptedException ie) {
         }
@@ -30,9 +28,6 @@ public class Runner extends Thread implements Runnable {
 
         System.out.println(runner3.getName() + " берет палочку");
         try {
-            Thread th3 = new Thread(runner3);
-            th3.start();
-            th3.join();
             sleep(5000);
         } catch (InterruptedException ie) {
         }
@@ -40,9 +35,6 @@ public class Runner extends Thread implements Runnable {
 
         System.out.println(runner4.getName() + " берет палочку");
         try {
-            Thread th4 = new Thread(runner4);
-            th4.start();
-            th4.join();
             sleep(5000);
         } catch (InterruptedException ie) {
         }
@@ -50,9 +42,6 @@ public class Runner extends Thread implements Runnable {
 
         System.out.println(runner5.getName() + " берет палочку");
         try {
-            Thread th5 = new Thread(runner5);
-            th5.start();
-            th5.join();
             sleep(5000);
         } catch (InterruptedException ie) {
         }
