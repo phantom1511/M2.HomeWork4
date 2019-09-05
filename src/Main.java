a0af2fc008ec7ee99b901d2d9d4df3f0123d786f
Dastan Tulokulov <dastan.tulokulov.kg@mail.ru>
2019-09-05 11:52:12 +0600

    Maybe prokatit)

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,10 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-            new Runner( " берет палочку " ).run();
+            new Runner( " Runner 1 ", "Runner 2 " ).run();
+            new Runner( " Runner 2 ", "Runner 3 " ).run();
+            new Runner( " Runner 3 ", "Runner 4 " ).run();
+            new Runner( " Runner 4 ", "Runner 5 " ).run();
+            new Runner( " Runner 5 ", "Runner 1" ).run();
     }
 }
--- src/Runner.java
+++ src/Runner.java
@@ -1,18 +1,25 @@
 public class Runner extends Thread {
-    public Runner(String name) {
+    String name2;
+
+    public String getName2() {
+        return name2;
+    }
+
+    public Runner(String name, String name2) {
         super(name);
+        this.name2 = name2;
     }
 
     public void run() {
-        System.out.println("Runner 1 берет палочку");
+        System.out.println(this.getName() + " берет палочку");
         try {
             sleep(5000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        System.out.println("Runner 1 бежит к Runner 2");
+        System.out.println(this.getName() + " бежит к " + name2);
 
-        System.out.println("Runner 2 берет палочку");
+        /*System.out.println("Runner 2 берет палочку");
         try {
             sleep(5000);
         } catch (InterruptedException e) {
@@ -42,7 +49,6 @@ public class Runner extends Thread {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        System.out.println("Runner 5 бежит к Runner 1");
-        System.out.println("Runner 1 берет палочку");
+        System.out.println("Runner 5 бежит к Runner 1");*/
     }
 }
