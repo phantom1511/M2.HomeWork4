df42ff65575dc06c2f17ae6ee80b12e9b954dc46
Dastan Tulokulov <dastan.tulokulov.kg@mail.ru>
2019-09-05 18:41:49 +0600

    finish

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,15 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Запуск главного потока " + Thread.currentThread().getName());
-        Runner runner = new Runner("Runner ");
+        Runner runner = new Runner();
         runner.start();
+        System.out.println("Завершение главного потока " + Thread.currentThread().getName());
 //        Thread th = new Thread(runner);
 //        th.start();
 //        try {
 //            th.join();
 //        } catch (InterruptedException ie) {
 //        }
-//        System.out.println("Завершение главного потока " + Thread.currentThread().getName());
+//
     }
 }
--- src/Runner.java
+++ src/Runner.java
@@ -1,51 +1,47 @@
 public class Runner extends Thread {
+    Thread runner1 = new Thread("Runner 1 ");
+    Thread runner2 = new Thread("Runner 2 ");
+    Thread runner3 = new Thread("Runner 3 ");
+    Thread runner4 = new Thread("Runner 4 ");
+    Thread runner5 = new Thread("Runner 5 ");
 
-    public Runner(String name) {
 
-        super(name);
-    }
 
     public void run() {
-        Thread runner1 = new Thread("Runner 1 ");
-        Thread runner2 = new Thread("Runner 2 ");
-        Thread runner3 = new Thread("Runner 3 ");
-        Thread runner4 = new Thread("Runner 4 ");
-        Thread runner5 = new Thread("Runner 5 ");
-
+        try{
         System.out.println(runner1.getName() + " берет палочку");
-        try {
-            sleep(5000);
-        } catch (InterruptedException ie) {
-        }
         System.out.println(runner1.getName() + " бежит к " + runner2.getName());
+        runner1.start();
+        sleep(5000);
+
+
 
         System.out.println(runner2.getName() + " берет палочку");
-        try {
-            sleep(5000);
-        } catch (InterruptedException ie) {
-        }
         System.out.println(runner2.getName() + " бежит к " + runner3.getName());
+        join();
+        runner2.start();
+        sleep(5000);
 
         System.out.println(runner3.getName() + " берет палочку");
-        try {
-            sleep(5000);
-        } catch (InterruptedException ie) {
-        }
         System.out.println(runner3.getName() + " бежит к " + runner4.getName());
+        join();
+        runner3.start();
+        sleep(5000);
 
         System.out.println(runner4.getName() + " берет палочку");
-        try {
-            sleep(5000);
-        } catch (InterruptedException ie) {
-        }
         System.out.println(runner4.getName() + " бежит к " + runner5.getName());
+        join();
+        runner4.start();
+        sleep(5000);
 
         System.out.println(runner5.getName() + " берет палочку");
-        try {
-            sleep(5000);
+        System.out.println(runner5.getName() + " бежит к " + runner1.getName());
+        join();
+        runner5.start();
+        sleep(5000);
+
+        System.out.println(runner1.getName() + " берет палочку");
         } catch (InterruptedException ie) {
         }
-        System.out.println(runner5.getName() + " бежит к " + runner1.getName());
-        System.out.println(runner1.getName() + " берет палочку");
     }
 }
