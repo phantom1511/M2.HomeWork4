9a01b73ec7123388ff697296c3217335b1ba65ce
Dastan Tulokulov <dastan.tulokulov.kg@mail.ru>
2019-09-05 17:54:20 +0600

    Maybe prokatit)

--- src/Runner.java
+++ src/Runner.java
@@ -17,35 +17,35 @@ public class Runner extends Thread {
             sleep(5000);
         } catch (InterruptedException ie) {
         }
-        System.out.println(runner1.getName()+ " бежит к " + runner2.getName());
+        System.out.println(runner1.getName() + " бежит к " + runner2.getName());
 
         System.out.println(runner2.getName() + " берет палочку");
         try {
             sleep(5000);
         } catch (InterruptedException ie) {
         }
-        System.out.println(runner2.getName()+ " бежит к " + runner3.getName());
+        System.out.println(runner2.getName() + " бежит к " + runner3.getName());
 
         System.out.println(runner3.getName() + " берет палочку");
         try {
             sleep(5000);
         } catch (InterruptedException ie) {
         }
-        System.out.println(runner3.getName()+ " бежит к " + runner4.getName());
+        System.out.println(runner3.getName() + " бежит к " + runner4.getName());
 
         System.out.println(runner4.getName() + " берет палочку");
         try {
             sleep(5000);
         } catch (InterruptedException ie) {
         }
-        System.out.println(runner4.getName()+ " бежит к " + runner5.getName());
+        System.out.println(runner4.getName() + " бежит к " + runner5.getName());
 
         System.out.println(runner5.getName() + " берет палочку");
         try {
             sleep(5000);
         } catch (InterruptedException ie) {
         }
-        System.out.println(runner5.getName()+ " бежит к " + runner1.getName());
+        System.out.println(runner5.getName() + " бежит к " + runner1.getName());
         System.out.println(runner1.getName() + " берет палочку");
     }
 }
